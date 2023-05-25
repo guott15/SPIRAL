4fc3ba1eea40f43e02d4f584e3d5ba386d5743f1
guott15 <576289054@qq.com>
2023-05-25 14:44:49 +0800

    0525

--- spiral/utils.py
+++ spiral/utils.py
@@ -43,6 +43,23 @@ from torch.utils.data import Dataset,TensorDataset,DataLoader
 
 # In[128]:
 
+def refine(sample_id, pred, dis, num_nbs=8):
+    refined_pred=[]
+    pred=pd.DataFrame({"pred": pred}, index=sample_id)
+    dis_df=pd.DataFrame(dis, index=sample_id, columns=sample_id)
+    for i in range(len(sample_id)):
+        index=sample_id[i]
+        dis_tmp=dis_df.loc[index, :].sort_values()
+        nbs=dis_tmp[0:num_nbs+1]
+        nbs_pred=pred.loc[nbs.index, "pred"]
+        self_pred=pred.loc[index, "pred"]
+        v_c=nbs_pred.value_counts()
+        if (v_c.loc[self_pred]<(num_nbs+1)/2) and (np.max(v_c)>(num_nbs+1)/2):
+            refined_pred.append(v_c.idxmax())
+        else:           
+            refined_pred.append(self_pred)
+    return refined_pred
+
 
 def process_adj(edges,n,loop=False,normalize_adj=False):
     m=edges.shape[0]
